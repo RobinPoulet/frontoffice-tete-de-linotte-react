ae6ca70c3af3baf73ae1cb4a68eb53e5dfbc17e3
Dev2 <dev2@iMac-de-Dev2.local>
2023-03-02 17:28:49 +0100

    change template

--- src/components/SingleItem/index.jsx
+++ src/components/SingleItem/index.jsx
@@ -1,40 +1,30 @@
 import React from 'react'
-import { Card, Col, Badge, Row } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SingleItem = ({ product }) => {
   const { avatarUrl, description, name, inStock, price } = product
   return (
-    <Card border="dark" lg={8} className="mx-auto p-4 w-75" bg="light">
-      <Row>
-        <Col lg={6} className="mx-auto">
-          <Card.Img
-            src={avatarUrl}
-            className="card-img"
-            style={{ width: '200px', height: '200px' }}
-          />
-        </Col>
-        <Col lg={6} className="mx-auto">
-          <Card.Title className="h1">{name}</Card.Title>
-          <br />
-          <Card.Body>
-            <Card.Text>{description}</Card.Text>
-            <Card.Text>
-              {inStock ? (
-                <Badge pill bg="success" className="mb-2">
-                  Disponible
-                </Badge>
-              ) : (
-                <Badge pill bg="danger" className="mb-2">
-                  Non disponible
-                </Badge>
-              )}
-            </Card.Text>
-            <Card.Text>{price} €</Card.Text>
-          </Card.Body>
-        </Col>
-      </Row>
-    </Card>
+    <div className="col">
+      <div className="card shadow-sm">
+        <img
+          className="card-img-top bg-dark cover"
+          height="200"
+          alt=""
+          src={avatarUrl}
+        />
+        <div className="card-body">
+          <h5 className="card-title text-center text-dark text-truncate">
+            {name}
+          </h5>
+          <p className="card-text text-center text-muted mb-0">{price}</p>
+          <div className="d-grid d-block">
+            <button className="btn btn-outline-dark mt-3">
+              <FontAwesomeIcon icon={['fas', 'cart-plus']} /> Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
-
 export default SingleItem
